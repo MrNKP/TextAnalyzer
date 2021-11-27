d72b9725163770c22a8e94528af09c554acf9439
Alex <ledkast@gmail.com>
2021-11-27 20:57:03 +0300

    Significantly improve collocations processing

--- Analyzer.py
+++ Analyzer.py
@@ -68,7 +68,7 @@ def words_processing(text):
     print(GREEN + BOLD + 'Most common words:' + END)
     for word in freq_dist.most_common(15):
         if (word[1] > 1):
-            print('\t', word, morph.parse(word[0])[0].tag.POS)
+            print('\t', word)
 
 
 # Определения
@@ -108,7 +108,68 @@ def definition_processing(text):
         position += 1
     print(GREEN + BOLD + 'Definitions: ' + END)
     for definition in definitions:
-        print('\t', definition)
+        print('\t>>> ', definition)
+
+
+def parse_and_inflect_collocations(word_tokens):
+    collocations_tokens = list()
+    i = 0
+    while i < (len(word_tokens) - 1):
+        p1 = morph.parse(word_tokens[i])[0]
+        pos1 = p1.tag.POS
+
+        p2 = morph.parse(word_tokens[i + 1])[0]
+        pos2 = p2.tag.POS
+
+        if (
+                # сущ + прил
+                (pos1 == 'ADJF' and pos2 == 'NOUN') or
+                (pos1 == 'NOUN' and pos2 == 'ADJF') or
+
+                # глаг + сущ
+                (pos1 == 'VERB' and pos2 == 'NOUN') or
+                (pos1 == 'INFN' and pos2 == 'NOUN') or
+                (pos1 == 'NOUN' and pos2 == 'VERB') or
+                (pos1 == 'NOUN' and pos2 == 'INFN')
+        ) and p1.normal_form != p2.normal_form and len(p1.word) > 1 and len(p2.word) > 1:
+            if pos1 == 'NOUN':  # определяем положение сущ
+                p1_normal = morph.parse(p1.normal_form)[0]
+                gender1 = p1_normal.tag.gender  # род
+                number1 = p1_normal.tag.number  # число
+                case1 = p1_normal.tag.case  # падеж
+                if pos2 == 'ADJF':
+                    (word2, error) = try_inflect_word(p2, gender1, number1, case1)
+                    collocations_tokens.append((p1.word if error else p1_normal.word) + ' ' + word2)
+                # else:
+                #     collocations_tokens.append(p1.word + ' ' + try_inflect_word(p2, gender1, number1, case1)[0])
+            else:
+                p2_normal = morph.parse(p2.normal_form)[0]
+                gender2 = p2_normal.tag.gender
+                number2 = p2_normal.tag.number
+                case2 = p2_normal.tag.case
+                if pos1 == 'ADJF':
+                    (word1, error) = try_inflect_word(p1, gender2, number2, case2)
+                    collocations_tokens.append(word1 + ' ' + (p2.word if error else p2_normal.word))
+                else:
+                    collocations_tokens.append(try_inflect_word(p1, gender2, number2, case2)[0] + ' ' + p2.word)
+        i = i + 1
+    return collocations_tokens
+
+
+def try_inflect_word(parse, gender, number, case):
+    if gender is not None:
+        parse = parse.inflect({gender})
+    if number is not None:
+        parse = parse.inflect({number})
+    if case is not None:
+        parse = parse.inflect({case})
+
+    return parse.word, False
+    # if gender is None or number is None or case is None or parse.inflect({gender, number, case}) is None:
+    #     return parse.word, True
+    # else:
+    #     return parse.inflect({gender, number, case}).word, False
+
 
 # Словосочетания
 def collocation_processing(text):
@@ -118,56 +179,22 @@ def collocation_processing(text):
 
     word_tokens = word_tokenize(text, 'russian')
 
-    # TODO вынести в метод
-    #   базовыми словосочетаниями считаем сущ + прил и глаг + суш
-    #   два слова подряд нельзя в словосочетании
-    #   для приведения в норм форму - определяем род,число сущ -> приводим к нему прилаг (род, число) или глаг (род) с помощью morphy2
-    #  ADJF + NOUN <
-    #  VERB\INFN + NOUN <
-
-    # доп виды
+    # Словосочетания из 2-х слов - <сущ + прил> или <глаг + сущ>
+    # виды словосочетаний, которые не учитываем
     #  NOUN + NOUN
     #  NUMR + NOUN
     #  VERB\INFN + ADVB\GRND
     #  GRND + NOUN/ADVB
     #  PRTF/PRTS + зависимое слово?
-    collocations_tokens = list()
-    i = 0
-    while i < (len(word_tokens) - 1):
-        p1 = morph.parse(word_tokens[i])[0]
-        pos1 = p1.tag.POS
-        p2 = morph.parse(word_tokens[i+1])[0]
-        pos2 = p2.tag.POS
-
-        if (
-                (pos1 == 'ADJF' and pos2 == 'NOUN') or
-                (pos1 == 'NOUN' and pos2 == 'NOUN') or
-                (pos1 == 'NUMR' and pos2 == 'NOUN') or
-                (pos1 == 'VERB' and pos2 == 'NOUN') or
-                (pos1 == 'INFN' and pos2 == 'NOUN') or
-                (pos1 == 'VERB' and pos2 == 'ADVB') or
-                (pos1 == 'INFN' and pos2 == 'ADVB') or
-                (pos1 == 'VERB' and pos2 == 'GRND') or
-                (pos1 == 'INFN' and pos2 == 'GRND') or
-                (pos1 == 'GRND' and pos2 == 'NOUN') or
-                (pos1 == 'GRND' and pos2 == 'ADVB')
-        ) and len(p1.word) > 1 and len(p2.word) > 1:
-            collocations_tokens.append(p1.word + ' ' + p2.word)
-        i = i + 1
-
-    # очистка текста
-    rus_stopwords = stopwords.words('russian')
-    rus_stopwords += []  # при необходимости добавить доп слова не несущие смысловой нагрузки
-    collocations_tokens = [token for token in collocations_tokens if token not in rus_stopwords]
+    collocations_tokens = parse_and_inflect_collocations(word_tokens)
 
     # результат
     nltk_text = nltk.Text(collocations_tokens)
     freq_dist = FreqDist(nltk_text)  # распределение частот
     print(GREEN + BOLD + 'Most common collocations:' + END)
-    for word in freq_dist.most_common(15):
-        if (word[1] > 1):
-            print('\t', word, morph.parse(word[0])[0].tag.POS)
-
+    for collocation in freq_dist.most_common(15):
+        if (collocation[1] > 1):
+            print('\t', collocation)
 
 
 if __name__ == '__main__':
